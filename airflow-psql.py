816dd767ff4034a397f3c79e61c54e3f200c5b4a
amal209 <elhadyne.amal@gmail.com>
2022-04-07 15:30:52 +0000

    postgresql dag

--- airflow-psql.py
+++ airflow-psql.py
@@ -17,7 +17,6 @@ with DAG(
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
-    start_date=days_ago(2),
     start_date=pendulum.datetime(2022, 4, 7, tz="UTC"),
     tags=['example'],
 ) as dag:
