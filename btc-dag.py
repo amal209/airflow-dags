6e5d53ae2afefc79575d296c497913b0c01bb160
amal209 <elhadyne.amal@gmail.com>
2022-04-12 13:16:50 +0000

    btc price dag

--- btc-dag.py
+++ btc-dag.py
@@ -1,4 +1,9 @@
 from asyncio import Task
+from datetime import date
+from email.errors import CloseBoundaryNotFoundDefect
+from msilib.schema import tables
+
+from openpyxl import load_workbook
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import pendulum
@@ -23,12 +28,26 @@ with DAG(
     #schedule_interval=timedelta(days=1),
     start_date=pendulum.datetime(2022, 4, 12, tz="UTC"),
 ) as dag:
-    task1 = PythonOperator(
+    # task1 ===>  Extract data
+    extract_data = PythonOperator(
         task_id='extract_data',
         python_callable=extractData,
         dag=dag,
     )
+    # task2 ===>  create table
+    create_table = PostgresOperator(
+        task_id="create_table_btc_price",
+        postgres_conn_id='airflow-postgresql',
+        sql="""
+            CREATE TABLE IF NOT EXISTS btc_price (
+            id SERIAL PRIMARY KEY,
+            Datetime DATE NOT NULL,
+            Open FLOAT NOT NULL,
+            High FLOAT NOT NULL,
+            Low FLOAT NOT NULL,
+            Close FLOAT NOT NULL);
+          """,
+    )
 
-
-
-task1
+#Order of tasks 
+extract_data >> create_table
