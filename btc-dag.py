bbbb6b8a5fcd2d3d3bb058872c7cfd706a1b781d
amal209 <elhadyne.amal@gmail.com>
2022-04-13 12:37:52 +0000

    btc price dag

--- btc-dag.py
+++ btc-dag.py
@@ -29,7 +29,7 @@ def transform():
     # read csv
     data = pd.read_csv("/tmp/data.csv")
     #print("reading csv",data)
-    price_df = pd.DataFrame(data , columns = ["Open", "High", "Low" , "Close"])
+    price_df = pd.DataFrame(data , columns = ["Datetime" , "Open", "High", "Low" , "Close"])
     print("PRICE DATAFRAME : ",price_df)
 
     
