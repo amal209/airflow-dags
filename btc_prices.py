e7af622c0e505644d813cc572568418845ea864c
amal209 <elhadyne.amal@gmail.com>
2022-06-26 00:18:33 +0100

    schedule interval

--- btc_prices.py
+++ btc_prices.py
@@ -57,7 +57,7 @@ with DAG(
     default_args=default_args,
     description='Getting the BTC price from Yahoo',
     #schedule_interval=timedelta(days=1),
-    start_date=pendulum.datetime(2022, 5, 30, tz="UTC"),
+    start_date=pendulum.datetime(2022, 6, 26, tz="UTC"),
 ) as dag:
  
     # task1 ==> extract data
