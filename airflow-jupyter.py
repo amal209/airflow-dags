78ebde52288ef472f0a51bcf0e49867b3cb10bad
amal209 <elhadyne.amal@gmail.com>
2022-08-15 12:30:14 +0100

    reading notebooks

--- airflow-jupyter.py
+++ airflow-jupyter.py
@@ -5,27 +5,24 @@ from airflow.providers.papermill.operators.papermill import PapermillOperator
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=1)
+    'retries':2
 }
+
+
 
 with DAG(
     dag_id='example_papermill_operator',
     default_args=default_args,
-    schedule_interval='0 0 * * *',
     start_date=datetime(2022, 8, 15),
     #template_searchpath='/usr/local/airflow/include',
     #template_searchpath='/dags_airflow-dags',
-    catchup=False
-) as dag_1:
-
+) as dag:
     notebook_task = PapermillOperator(
         task_id="run_example_notebook",
         input_nb="/dags_airflow-dags/test.ipynb",
         output_nb="/dags_airflow-dags/out-{{ execution_date }}.ipynb",
         parameters={"execution_date": "{{ execution_date }}"},
     )
+
+#order 
+run_example_notebook
