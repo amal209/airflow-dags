13cf78e972574bd7e75d9b37e032a2a91d515a23
amal209 <elhadyne.amal@gmail.com>
2022-08-23 12:34:04 +0100

    bash operator

--- airflow-jupyterv2.py
+++ airflow-jupyterv2.py
@@ -20,7 +20,7 @@ with DAG(
     dag_id='example_bach_operator',
     default_args=default_args,
     start_date=datetime(2022, 8, 23),
-    schedule_interval='@daily',
+    #schedule_interval='@daily',
 ) as dag:
     t2 = BashOperator(task_id='test_bash_operator',bash_command=command, dag=dag)
 
